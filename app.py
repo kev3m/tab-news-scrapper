b375bdb70a68ea52c17a13ee7be47aa56cbc8c8b
Keven C <keven_jkc@hotmail.com>
2023-06-30 11:23:06 -0300

    add: first dataset for test

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from scrapper.article import Article
 
 
 scraper = ArticleScrapper()
+
+# Create a dataframe of articles
 articles_list = scraper.createArticleList()
+df = scraper.createArticleDataFrame(articles_list).fillna(0) #Fillna = replace all none values to 0
+df.to_csv('./datasets/article_dataset.csv')
 
-
-df = scraper.createArticleDataFrame(articles_list)
-
-print(df)
 
