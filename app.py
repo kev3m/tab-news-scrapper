88018b9ad62dd1f8cd4514d7f62acf451e9a22ae
Keven C <keven_jkc@hotmail.com>
2023-07-13 15:41:38 -0300

    feat: add reindex function to ensure that index 0 is always the first article.

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ from scrapper.articlescrapper import ArticleScrapper
 from scrapper.Article_model import Article
 
 
+STRATEGY = 'new' # new | relevant | old
+
 scraper = ArticleScrapper()
 
 if __name__ == '__main__':
     # Create a dataframe of articles
     articles_list = scraper.createArticleList()
     df = scraper.createArticleDataFrame(articles_list).fillna(0) #Fillna = replace all none values to 0
-    df.to_csv('./datasets/article_dataset.csv')
+
+    df = df.reindex(index=df.index[::-1]) #inverting the index
+
+    df.to_csv('./datasets/test_article_dataset.csv')
     print("Dataframe successfully updated!")
 
